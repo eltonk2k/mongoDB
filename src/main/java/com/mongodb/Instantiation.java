7582deb6564fe0ad05787e87159a6c86f2a1b302
eltonk2k <elieltonpb1@gmail.com>
2021-08-08 22:02:21 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/mongodb/Instantiation.java
+++ src/main/java/com/mongodb/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.mongodb.domain.Post;
 import com.mongodb.domain.User;
+import com.mongodb.dto.AuthorDTO;
 import com.mongodb.repository.PostRepository;
 import com.mongodb.repository.UserRepository;
 
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"),  "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),  "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
